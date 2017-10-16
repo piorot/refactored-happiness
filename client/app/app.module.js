984dc2b64fb81feb04294654b5aac2a5631305f3
piorot <rotynski@gmail.com>
2017-10-16 21:32:04 +0200

    CHG : ui-router starts working very basic stuff

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -1,4 +1,17 @@
 (function () {
     "use strict";
-    angular.module('app', ['ngMaterial', 'menu', 'toolbar']);
+    angular
+        .module('app', ['ngMaterial', 'ui.router', 'menu', 'toolbar'])
+        .config(['$logProvider', '$stateProvider', '$locationProvider', function($logProvider, $stateProvider, $locationProvider){
+            //$logProvider,debugEnabled(true);
+
+            $stateProvider
+            .state('home', {
+                url: '/',
+                template: '<h1>This is an inline template</h1>'
+            });
+
+            $locationProvider.hashPrefix('');
+
+        }])
 })();
