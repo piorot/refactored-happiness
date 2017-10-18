a4659f304647bb1e9369a2ea5fc94948398f0ce5
piorot <rotynski@gmail.com>
2017-10-18 17:43:28 +0200

    CHG : Very basic test

---
+++ client/app/menu/menu.controller.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('jakis opis', function () {
+
+    beforeEach(module('ui.router'));
+    beforeEach(module('menu'));
+    
+    describe('menu controller', function () {
+
+        it('should ....', inject(function ($controller) {
+            //spec body
+            var menuController = $controller('MenuController');
+            expect(menuController).toBeDefined();
+        }));
+
+    });
+});
