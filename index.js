fc604d81db9d4f3a651573a3bb7fe149e6f93af4
piorot <rotynski@gmail.com>
2017-10-29 13:19:46 +0100

    CHG : slight improvements

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 "use strict";
 
 const HttpServer = require("http-server");
-const deployd = require('deployd');
 const port = process.env.port || 8000;
 const options = { root: './client',cache: -1 };
 
 let server = HttpServer.createServer(options);
-let deploydOptions = {
-    port: port,
-    env: 'production',
-    db: {
-      host: 'ds237445.mlab.com',
-      port: 37445,
-      name: 'heroku_p4nmksq9',
-      credentials: {
-        username: 'piorot',
-        password: 'Test123'
-      }
-    }
-};
-
-
 startServer();
 
 
 function startServer(){
     
-    server = deployd.attach(server, deploydOptions)
     server.listen();
     console.log("server started");
 }
