567d7b647148f546018b22064e401edb50bd70cd
piorot <rotynski@gmail.com>
2017-11-02 22:16:23 +0100

    CHG project List controller unit tests

--- client/app/menu/menu.controller.spec.js
+++ client/app/menu/menu.controller.spec.js
@@ -9,8 +9,8 @@ describe('Menu Controller', function () {
     beforeEach(module('ui.router'));
     beforeEach(module('menu'));
     beforeEach(inject(function (_$controller_, _$state_) {
-        $controller = _$controller_;
         $state = _$state_;
+        $controller = _$controller_;
     }));
 
     it("should change state when called", function() {
---
+++ client/app/projects/projects-list.controller.spec.js
@@ -0,0 +1,53 @@
+'use strict';
+
+describe('ProjectsListController', function () {
+    var $q;
+    var $state;
+    var $scope;
+    var sandbox;
+    var $rootScope;
+    var $controller;
+    var projectsService;
+    var baseBackendUrl;
+    var peopleListResolverService;
+
+
+
+    beforeEach(module('ui.router'));
+    beforeEach(module('people'));
+    beforeEach(module('app'));
+    beforeEach(inject(function (_$controller_, _projectsService_, _baseBackendUrl_, _peopleListResolverService_, _$rootScope_, _$q_, _$state_) {
+        $q = _$q_;
+        $state = _$state_;
+        $rootScope = _$rootScope_;
+        $controller = _$controller_;
+        $scope = _$rootScope_.$new();
+        baseBackendUrl = _baseBackendUrl_;
+        projectsService = _projectsService_;
+        peopleListResolverService = _peopleListResolverService_;
+    }));
+
+    it("on load should download projectsList", function () {
+        sandbox = sinon.sandbox.create();
+        var getProjectsList = sandbox.stub(projectsService, "getProjectsList").resolves({ data: [] });
+
+        var ProjectsListController= $controller("ProjectsListController", { projectsService: projectsService, baseBackendUrl: baseBackendUrl, $scope: $scope });
+
+        expect(getProjectsList).to.have.been.calledOnce;
+        expect(getProjectsList).to.have.been.calledWith();
+    })
+
+    it("should change stateto projectsDetails when called showProjectDetails", function() {
+        var ProjectsListController = $controller('ProjectsListController', { $state: $state });
+        var dummyProjectMock = {id : 123};
+        var sandbox = sinon.sandbox.create();
+        var stateGo = sandbox.stub($state, "go");
+        
+        ProjectsListController.showProjectDetails(dummyProjectMock);
+        expect(stateGo).to.have.been.calledOnce;
+        expect(stateGo).to.have.been.calledWith('projectsDetails', {id : dummyProjectMock.id});
+
+    })
+
+   
+});
